f709d7a6b25a768efa77f820ebe26723ec1055df
Lucas Duran <100479433+lucasduran3@users.noreply.github.com>
2023-09-05 23:05:49 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Phaser from "phaser";
+
+import HelloWorldScene from "./scenes/HelloWorldScene";
+import UI from "./scenes/UI";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 800,
+      height: 600,
+    },
+    max: {
+      width: 1600,
+      height: 1200,
+    },
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+      debug: false,
+    },
+  },
+  scene: [HelloWorldScene, UI],
+};
+
+export default new Phaser.Game(config);
---
+++ src/scenes/HelloWorldScene.js
@@ -0,0 +1,70 @@
+import Phaser from "phaser";
+import events from "./EventCenter";
+
+// Manejador de eventos centralizados para comunicacion de componentes
+
+// Importacion
+// import events from './EventCenter'
+
+// Emisor de mensaje de difusion
+// Recibe el nombre del mensaje y los valores de parametro
+// events.emit('health-changed', this.health)
+
+// Receptor de mensaje, por ejemplo escena de UI
+// Recibe el nombre del mensaje y una funcion callback a ejecutar
+// events.on('health-changed', this.handleHealthChanged, this)
+
+export default class HelloWorldScene extends Phaser.Scene {
+  constructor() {
+    super("hello-world");
+  }
+
+  preload() {
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.atlas(
+      "match3",
+      "assets/atlas/match3.png",
+      "assets/atlas/match3.json"
+    );
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+
+    const emitter = this.add.particles(0, 0, "match3", {
+      frame: ["Match3_Icon_30", "Match3_Icon_29"],
+      lifespan: 4000,
+      speed: { min: 200, max: 350 },
+      scale: { start: 0.4, end: 0 },
+      rotate: { start: 0, end: 360 },
+      gravityY: 200,
+      emitting: false,
+    });
+
+    const logo = this.physics.add.image(400, 100, "logo");
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.emitParticleAt(logo.x, logo.y, 4);
+
+    // add green rectangle for collider and asign physics
+    const floor = this.add.rectangle(400, 600, 800, 20, 0x00ff00);
+    this.physics.add.existing(floor, true);
+
+    // launch UI scene
+    this.scene.launch("ui");
+
+    // add collider with floor
+    this.physics.add.collider(logo, floor, () => {
+      console.log("collider-event");
+
+      // Event emitter
+      events.emit("collider-event", {
+        fecha: new Date().toLocaleTimeString(),
+      });
+    });
+  }
+}
