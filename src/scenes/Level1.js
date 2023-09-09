2c3982c374bbf841e2077d2fbc7c1dc8f5e6ff94
Federico Degiovanni <federicoadegiovanni@gmail.com>
2023-09-09 17:39:19 -0300

    fix angles

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -1,38 +1,48 @@
 import Phaser from "phaser";
 import Player from "../components/Player";
 
-export default class Level1 extends Phaser.Scene{
-    constructor(){
-        super("Level1");
-    }
+export default class Level1 extends Phaser.Scene {
+  constructor() {
+    super("Level1");
+  }
 
-    create(){
-        this.map = this.make.tilemap({key: "map"});
-        const floorL = this.map.addTilesetImage("floor", "floor");
-        const wallL = this.map.addTilesetImage("wall", "wall");
-        const floorLayer = this.map.createLayer("floor", floorL, 0,0);
-        const wallLayer = this.map.createLayer("wall", wallL, 0,0);
-        const objectsLayer = this.map.getObjectLayer("objects");
+  create() {
+    this.map = this.make.tilemap({ key: "map" });
+    const floorL = this.map.addTilesetImage("floor", "floor");
+    const wallL = this.map.addTilesetImage("wall", "wall");
+    const floorLayer = this.map.createLayer("floor", floorL, 0, 0);
+    const wallLayer = this.map.createLayer("wall", wallL, 0, 0);
+    const objectsLayer = this.map.getObjectLayer("objects");
 
-        console.log(floorLayer);
-        console.log(objectsLayer);
+    console.log(floorLayer);
+    console.log(objectsLayer);
 
-        const spawnPoint = this.map.findObject(
-            "objects",
-            (obj) => obj.name === "player"
-        );
+    const spawnPoint = this.map.findObject(
+      "objects",
+      (obj) => obj.name === "player"
+    );
 
-        this.player = new Player(this, spawnPoint.x, spawnPoint.y, "player");   
-        
-        this.cameras.main.startFollow(this.player);
-        this.physics.world.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels);
-        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.player = new Player(this, spawnPoint.x, spawnPoint.y, "player");
 
-        wallLayer.setCollisionByProperty({colision:true});
-        this.physics.add.collider(wallLayer, this.player);
-    }
+    this.cameras.main.startFollow(this.player);
+    this.physics.world.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
 
-    update(){
-        this.player.update();
-    }
+    wallLayer.setCollisionByProperty({ colision: true });
+    this.physics.add.collider(wallLayer, this.player);
+  }
+
+  update(time, delta) {
+    this.player.update(time, delta);
+  }
 }
