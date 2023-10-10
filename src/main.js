eb52084e87795b753d6c7810f993778a14716939
lucasduran3 <lucasduran510@gmail.com>
2023-10-10 16:34:22 -0300

    api traducila

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import GameOver from "./scenes/GameOver";
 import Prologue from "./scenes/Prologue";
 import Lobby from "./scenes/Lobby";
 import GameWin from "./scenes/GameWin";
+import SelectLang from "./scenes/SelectLang";
 
 
 const config = {
@@ -34,10 +35,10 @@ const config = {
     default: "arcade",
     arcade: {
       gravity: { y: 0 },
-      debug: false,
+      debug: true,
     },
   },
-  scene: [Preload, MainMenu, Help, LevelSelector,Prologue, Lobby, Level1, UI, Pause, Controls, PowerUp, GameOver, GameWin],
+  scene: [Preload, SelectLang, MainMenu, Help, LevelSelector,Prologue, Lobby, Level1, UI, Pause, Controls, PowerUp, GameOver, GameWin],
 };
 
 export default new Phaser.Game(config);
